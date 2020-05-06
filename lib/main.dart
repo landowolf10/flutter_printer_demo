ac616f177c3d136d7acb52090f2ccf42d4b83bf7
lando10 <orlando.avila@tortoise.dev>
2020-05-05 19:45:48 -0500

    Ejemplo de creación de PDF.

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    const title = 'Printing Demo';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(title),
+        ),
+        body: Center(
+          child: IconButton(
+            icon: const Icon(Icons.print),
+            onPressed: _printDocument,
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _printDocument() {
+    Printing.layoutPdf(
+      onLayout: (pageFormat) {
+        final doc = pw.Document();
+
+        doc.addPage(
+          pw.Page(
+            build: (pw.Context context) => pw.Center(
+              child: pw.Text('Hello World!'),
+            ),
+          ),
+        );
+
+        return doc.save();
+      },
+    );
+  }
+}
